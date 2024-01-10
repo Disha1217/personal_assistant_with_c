06c9863c8d200afd57f3cfaa2614e207954c3aee
Disha1217 <144909331+Disha1217@users.noreply.github.com>
2024-01-10 14:50:02 +0530

    Add files via upload

---
+++ personalassistant.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<windows.h>
+#include<windowsx.h>
+#include<mmsystem.h>
+
+
+int main()
+{
+char x[100];
+printf("*********************************************YOUR PERSONAL ASSISTANT ***************************************************");
+
+gets(x);
+ if(strcmp(ch,"hi")==0)
+{
+   PlaySound(TEXT("filename.way"),NULL,SND_SYNC);
+}
+ else if(strcmp(ch,"how are you")==0)
+{
+   PlaySound(TEXT("filename.way"),NULL,SND_SYNC);
+}
+
+
+
+
+
+
+
+
+
+
+}
